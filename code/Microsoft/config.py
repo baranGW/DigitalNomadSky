a5ee26dc4967a044fde4ea71772a4ad8774a8bd8
Baran Bugday <baran.bugday@hva.nl>
2026-05-18 16:15:25 +0200

    my azure credentials

--- code/Microsoft/config.py
+++ code/Microsoft/config.py
@@ -1,11 +1,11 @@
 #Parameters to find your VM. 
-tenantid = "78ba35ee-470e-4a16-ba92-ad53510ad7f6"  # your tenant-id, this is used to find VMs.
+tenantid = "0907bb1e-21fc-476f-8843-02d09ceb59a7"  # your tenant-id, this is used to find VMs.
 
 #Parameters to upload your VM.
 location = "westeurope"  # The VM will be created in this location.
-destionationtenantid = "78ba35ee-470e-4a16-ba92-ad53510ad7f6" # the VM will be creaed in this tenant
-subscription_id = "41aff5e1-41c9-4509-9fcb-d761d7f33740" # the VM will be created in this subscription
-resource_group = "output" # the VM will be created in this resource group. 
+destionationtenantid = "0907bb1e-21fc-476f-8843-02d09ceb59a7" # the VM will be creaed in this tenant
+subscription_id = "a9979e3a-54dc-43b9-8334-f70e19842a54" # the VM will be created in this subscription
+resource_group = "VirtualMachineTests" # the VM will be created in this resource group. 
 
-storage_account_name = "compliceert20"   # a temp storage account to upload the disk file. 
-container_name = "vhds"  # the temp container name to upload the disk file.  
+storage_account_name = "vmteststoragebaran"   # a temp storage account to upload the disk file. 
+container_name = "$logs"  # the temp container name to upload the disk file.  
